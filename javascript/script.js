7f587254438928d01fbfb2cda204c5128d7e6035
Avishay Pelach <avishay.pelach@gmail.com>
2017-02-28 01:26:15 +0200

    creating functions to create lists and cards

--- javascript/script.js
+++ javascript/script.js
@@ -1,18 +1,16 @@
 /**
  * Created by avishay on 27-Feb-17.
  */
-// let i = 1;
 function addingAList() {
-  // mNewObj = document.createElement('div');
-  // mNewObj.id = "BOX" + i;
-  // mNewObj.style.backgroundColor="red";
-  // mNewObj.style.visibility="show";
-  // mNewObj.innerHTML = "Box " + i;
-  // document.getElementById("tid").appendChild(mNewObj);
-  // alert(mNewObj.innerHTML);
-  // i++;
-  // create the list head add it to dad and give class
   const listHead = document.createElement('div');
   document.getElementById("tid").appendChild(listHead);
-  listHead.className = "panel panel-default";
+  const html = '<div class="panel panel-default"><div class="panel-heading panel-size"><h3 class="panel-title">Backlog</h3></div><div class="panel-body" id="addingCard"></div><div class="panel-footer panel-size"><button onclick="createACard()" class="addACard">add a card...</button></div>';
+  listHead.innerHTML = html;
 }
+function createACard() {
+  const listHead = document.createElement('div');
+  document.getElementById("addingCard").appendChild(listHead);
+  const html = '<div class="card"></div>';
+  listHead.innerHTML = html;
+}
+
