4d8b7f3302eda89ac89279cc61d578206ef937ce
Avishay Pelach <avishay.pelach@gmail.com>
2017-02-28 16:31:40 +0200

    creatig functions that replacing titles

--- javascript/script.js
+++ javascript/script.js
@@ -1,16 +1,91 @@
 /**
  * Created by avishay on 27-Feb-17.
  */
+//function that creates a new panel.
 function addingAList() {
-  const listHead = document.createElement('div');
-  document.getElementById("tid").appendChild(listHead);
-  const html = '<div class="panel panel-default"><div class="panel-heading panel-size"><h3 class="panel-title">Backlog</h3></div><div class="panel-body" id="addingCard"></div><div class="panel-footer panel-size"><button onclick="createACard()" class="addACard">add a card...</button></div>';
-  listHead.innerHTML = html;
+
+  const divHolder = document.createElement('div');
+
+  document.getElementById("tid").appendChild(divHolder);
+
+  const newList =
+
+  `<div class="panel panel-default">
+
+    <div class="panel-heading panel-size">
+    
+      <h3 class="panel-title">place holder</h3>
+      
+    </div>
+   
+    <div class="panel-body" id="cardList"> </div>
+  
+    <div class="panel-footer panel-size">
+  
+      <button onclick="createACard()" class="addACard">add a card...</button>
+  
+    </div>
+  
+  </div>`;
+
+  divHolder.innerHTML = newList;
 }
+//function that creates a card in in parent panel.
 function createACard() {
-  const listHead = document.createElement('div');
-  document.getElementById("addingCard").appendChild(listHead);
-  const html = '<div class="card"></div>';
-  listHead.innerHTML = html;
+
+  //target the corect list.
+  const target = event.target;
+  console.log(target);
+
+  //target the parent of the lists.
+  const currentPanel = target.parentNode.parentNode;
+  console.log(currentPanel);
+
+  //target right place to insert the new card.
+  const currentCardListHolder = currentPanel.querySelector(".panel-body");
+  console.log(currentCardListHolder);
+
+  //creating element that holds div creation.
+  const divHolder = document.createElement('div');
+  console.log(divHolder);
+
+  //giving divHolder style.
+  divHolder.className = "card";
+
+  //implementing divHolder into panel's body.
+  currentCardListHolder.appendChild(divHolder);
 }
+
+function divClicked() {
+  const h3Holder = document.querySelector(".panel-title");
+  console.log(h3Holder);
+
+  h3Holder.addEventListener('click', function () {
+
+  const editableText = $("<textarea />");
+
+  editableText.valueOf(h3Holder);
+
+  $(this).replaceWith(editableText);
+
+  editableText.focus();
+
+  // setup the blur event for this new textarea
+  editableText.blur(editableTextBlurred);
+  });
+}
+
+function editableTextBlurred() {
+  const html = $(this).val();
+  const viewableText = $("<h3>");
+  viewableText.html(html);
+  $(this).replaceWith(viewableText);
+  // setup the click event for this new div
+  $(viewableText).click(divClicked);
+};
+
+
+
+
+
 
